6373f18957c93e8ffc4b044fcd6b9da2ab831fbd
Marjelly <marjelly1@gmail.com>
2019-07-26 13:31:53 +0200

    I2C is working but only with 1 Byte

--- Arduino_Poti.ino
+++ Arduino_Poti.ino
@@ -1,19 +1,27 @@
+#include <Wire.h>
 
 int Poti=A0;
 long depthNumb=0;
+//byte depthNumbByte;
  
 void setup() {
   
  
  pinMode(Poti,INPUT);
  Serial.begin(9600);
-
+ Wire.begin(); 
+ 
 }
 
 void loop() {
   depthNumb=analogRead(Poti);
   depthNumb=depthNumb*488,28;
   Serial.println(depthNumb);
+ // depthNumbByte=depthNumb;
+  
+  Wire.beginTransmission(8); 
+  Wire.write(depthNumb);
+  Wire.endTransmission();    
    
   delay(100);
   
